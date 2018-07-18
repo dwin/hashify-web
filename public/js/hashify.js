809a8fc78bd7886b919698b9d9fdc7eb98b76bbb
Darwin Smith II <dwin@dlsmi.com>
2018-07-18 15:55:15 -0400

    Initial commit working file hashing

--- public/js/hashify.js
+++ public/js/hashify.js
@@ -1,3 +1,10 @@
+// Check for the various File API support.
+if (window.File && window.FileReader && window.FileList && window.Blob) {
+    // Great success! All the File APIs are supported.
+} else {
+    alert('The File APIs are not fully supported in this browser and you may not be able to use File hash feature.');
+}
+var baseURL = 'https://api.hashify.net';
 var app = new Vue({
     el: '.container',
     data: {
@@ -7,9 +14,9 @@ var app = new Vue({
                 method: '',
                 key: '',
                 requiredKeyLength:0,
-                endpoint:'',
                 dataSource: '',
                 digestFormat: '',
+                filename:'',
             },
         hashMethods: [{"Name":"Blake2b-256","Endpoint":"/hash/BLAKE2B-256","MinKeyLength":0,"MaxKeyLength":0},{"Name":"Blake2b-384","Endpoint":"/hash/BLAKE2B-384","MinKeyLength":0,"MaxKeyLength":0},{"Name":"Blake2b-512","Endpoint":"/hash/BLAKE2B-512","MinKeyLength":0,"MaxKeyLength":0},{"Name":"Blake2s-128","Endpoint":"/hash/BLAKE2s-128","MinKeyLength":0,"MaxKeyLength":0},{"Name":"Blake2s-256","Endpoint":"/hash/BLAKE2s-256","MinKeyLength":0,"MaxKeyLength":0},{"Name":"HighwayHash-256","Endpoint":"/hash/HIGHWAY","MinKeyLength":32,"MaxKeyLength":0},{"Name":"HighwayHash-64","Endpoint":"/hash/HIGHWAY-64","MinKeyLength":32,"MaxKeyLength":0},{"Name":"HighwayHash-128","Endpoint":"/hash/HIGHWAY-128","MinKeyLength":32,"MaxKeyLength":0},{"Name":"MD4","Endpoint":"/hash/MD4","MinKeyLength":0,"MaxKeyLength":0},{"Name":"MD5","Endpoint":"/hash/MD5","MinKeyLength":0,"MaxKeyLength":0},{"Name":"SHA1","Endpoint":"/hash/SHA1","MinKeyLength":0,"MaxKeyLength":0},{"Name":"SHA256","Endpoint":"/hash/SHA256","MinKeyLength":0,"MaxKeyLength":0},{"Name":"SHA384","Endpoint":"/hash/SHA384","MinKeyLength":0,"MaxKeyLength":0},{"Name":"SHA512","Endpoint":"/hash/SHA512","MinKeyLength":0,"MaxKeyLength":0},{"Name":"SHA512-256","Endpoint":"/hash/SHA512-256","MinKeyLength":0,"MaxKeyLength":0},{"Name":"SHA3-256","Endpoint":"/hash/SHA3-256","MinKeyLength":0,"MaxKeyLength":0},{"Name":"SHA3-384","Endpoint":"/hash/SHA3-384","MinKeyLength":0,"MaxKeyLength":0},{"Name":"SHA3-512","Endpoint":"/hash/SHA3-512","MinKeyLength":0,"MaxKeyLength":0}],
         errors:[{
@@ -22,7 +29,7 @@ var app = new Vue({
     computed:{
         keyLength: function() {
             var len = (new TextEncoder('utf-8').encode(this.userInput.key)).length
-            console.log("bytes: " + len)
+            //console.log("bytes: " + len)
             return  {
                 'is-valid': len =>  this.userInput.requiredKeyLength,
                 'is-invalid': len < this.userInput.requiredKeyLength
@@ -31,24 +38,51 @@ var app = new Vue({
     },
     methods:{
         getHashMethods: function() {
-            axios.get('https://api.hashify.net/methods')
+            axios.get(baseURL+'/methods')
                 .then(response => {
                 // JSON responses are automatically parsed.
                 this.hashMethods = response.data
                 })
                 .catch(e => {
                 this.errors.push({text:'Unable to connect to API server',level:'danger'})
-                })
+                });
         },
         submitPlaintext: function() {
             // Find Endpoint & Send Request
-            axios.post('https://api.hashify.net'+ this.getEndpoint()+'/'+this.userInput.digestFormat, this.userInput.plaintext, { 
+            axios.post(baseURL+ this.getEndpoint()+'/'+this.userInput.digestFormat, this.userInput.plaintext, { 
+            headers: {
+                'X-Hashify-Key': this.userInput.key
+            }}).then(response => {
+            // JSON responses are automatically parsed.
+            //this.result = response.data
+
+            this.results.unshift({Digest: response.data.Digest,DigestEnc: response.data.DigestEnc,Key: response.data.Key,Type: response.data.Type,Input: this.userInput.plaintext});
+
+            this.userInput.plaintext = '';
+            this.userInput.method = '';
+            this.userInput.dataSource = '';
+            this.userInput.digestFormat = '';
+            this.errors = [];
+            })
+            .catch(e => {
+            this.errors.push({text:'Unable to obtain hash digest',level:'danger'});
+            });
+                
+        },
+        submitFile: function() {
+            var formData = new FormData();
+            var formFile = document.querySelector('#file');
+            formData.append("file", formFile.files[0]);
+            // Find Endpoint & Send Request
+            axios.post(baseURL+ this.getEndpoint()+'/'+this.userInput.digestFormat, formData, {
                 headers: {
-                    'X-Hashify-Key': this.userInput.key
-                }}).then(response => {
+                'X-Hashify-Key': this.userInput.key,
+                'X-Hashify-Process':'multipart/form-data',
+                }
+            }).then(response => {
                 // JSON responses are automatically parsed.
                 //this.result = response.data
-                this.results.unshift({Digest: response.data.Digest,DigestEnc: response.data.DigestEnc,Key: response.data.Key,Type: response.data.Type,Input: this.userInput.plaintext});
+                this.results.unshift({Digest: response.data.Digest,DigestEnc: response.data.DigestEnc,Key: response.data.Key,Type: response.data.Type,Input: this.userInput.filename});
                 this.userInput.plaintext = '';
                 this.userInput.method = '';
                 this.userInput.dataSource = '';
@@ -56,45 +90,57 @@ var app = new Vue({
                 this.errors = [];
                 })
                 .catch(e => {
-                this.errors.push({text:'Unable to obtain hash digest',level:'danger'})
-                })
-                
+                this.errors.push({text:'Unable to obtain hash digest',level:'danger'});
+                });
         },
         requiresKey: function() {
-            var val = this.userInput.method
+            var val = this.userInput.method;
             var idx = this.hashMethods.findIndex(function(item, i){
-                return item.Name === val
+                return item.Name === val;
             });
             var reqLength = this.hashMethods[idx].MinKeyLength;
             this.userInput.requiredKeyLength = reqLength;
-            return (reqLength > 0)
+            return (reqLength > 0);
         },
         getEndpoint: function() {
-            var val = this.userInput.method
+            var val = this.userInput.method;
             var idx = this.hashMethods.findIndex(function(item, i){
-                return item.Name === val
+                return item.Name === val;
             });
-            return this.hashMethods[idx].Endpoint
+            return this.hashMethods[idx].Endpoint;
         },
         checkRandom: function() {
             if (! this.randomKey.generate) {
-                this.userInput.key = 'random'
+                this.userInput.key = 'random';
             } else {
-                this.userInput.key = ''
+                this.userInput.key = '';
             }
         },
         generateKey: function() {
-            axios.get('https://api.hashify.net/keygen/'+this.userInput.requiredKeyLength)
+            axios.get(baseURL+'/keygen/'+this.userInput.requiredKeyLength)
                 .then(response => {
                 // JSON responses are automatically parsed.
                 this.userInput.key = response.data.KeyHex
                 })
                 .catch(e => {
-                this.errors.push({text:'Unable to connect to API server',level:'danger'})
-                })
+                this.errors.push({text:'Unable to connect to API server',level:'danger'});
+                });
+        },
+        addFile: function(e) {
+            this.userInput.filename = e.target.files[0].name;
+            //var reader = new FileReader();
+            //var fileData = this.userInput.file;
+            /*
+            reader.onload = function(e) {
+                fileData = reader.result;
+                console.log(fileData);
+                app.userInput.plaintext = fileData;
+            };
+            reader.readAsBinaryString(e.target.files[0]);      
+            */
         },
       },
     mounted: function() {
         //this.getHashMethods();
     },
-})
+});
